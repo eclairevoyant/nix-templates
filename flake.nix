570bc71d272fe6bbdff02ae27c70b7f3d376438f
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-04-02 20:37:30 -0400

    add initial template

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "simple nix templates";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      flake = {
+        templates = {
+          nixos = {
+            path = ./nixos;
+            description = "minimal nixos template";
+          };
+        };
+      };
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          formatter = pkgs.nixfmt-rfc-style;
+        };
+    };
+}
