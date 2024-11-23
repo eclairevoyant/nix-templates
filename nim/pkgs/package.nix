a64b35f4496a192c3ed2b0a4c7606d6c1c8c0b2d
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-11-22 20:41:56 -0500

    nim: add package

--- nim/pkgs/package.nix
+++ nim/pkgs/package.nix
@@ -1,3 +1,47 @@
 {
+  lib,
+  buildNimPackage,
+  pandoc,
+}:
+let
+  fs = lib.fileset;
+in
+buildNimPackage (finalAttrs: rec {
+  pname = "FIXME";
+  version = "0.0.1";
 
-}
+  src = fs.toSource {
+    root = ./.;
+    fileset = fs.unions [
+      (fs.fileFilter (
+        file:
+        builtins.any file.hasExt [
+          "md"
+          "nim"
+        ]
+      ) ../src)
+    ];
+  };
+
+  nativeBuildInputs = [
+    pandoc
+  ];
+
+  hardeningEnable = [ "pie" ];
+
+  postBuild = ''
+    pandoc -s -o ${finalAttrs.pname}.1 doc/${pname}.man1.md
+  '';
+
+  postInstall = ''
+    install -Dm644 ${finalAttrs.pname}.1 -t "$out/share/man/man1/"
+  '';
+
+  meta = {
+    description = "FIXME";
+    homepage = "https://github.com/eclairevoyant/${pname}";
+    license = lib.licenses.cc-by-nc-sa-40;
+    mainProgram = pname;
+    platforms = lib.platforms.linux;
+  };
+})
