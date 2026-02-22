ec7ad6bd4350b6ccdfc90cc81333fdf5d8b0f483
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2026-02-22 00:38:46 -0500

    use lockable tarballs to speed up input fetching

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "simple nix templates";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
   };
 
   outputs =
