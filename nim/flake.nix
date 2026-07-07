10134987683e8424e118092216a974b2a051df26
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2026-07-06 22:23:19 -0400

    nim: support additional systems

--- nim/flake.nix
+++ nim/flake.nix
@@ -4,30 +4,42 @@
   outputs =
     { nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowlistedLicenses = with nixpkgs.lib.licenses; [ cc-by-nc-sa-40 ];
-      };
+      pkgs' =
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowlistedLicenses = with nixpkgs.lib.licenses; [ cc-by-nc-sa-40 ];
+        };
+      generateSystems =
+        f:
+        nixpkgs.lib.attrsets.genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (
+          system: f (pkgs' system)
+        );
     in
     {
-      formatter.${system} = pkgs.writeShellApplication {
-        name = "nimpretty-nixfmt-wrapper";
+      formatter = generateSystems (pkgs: {
+        default = pkgs.writeShellApplication {
+          name = "nimpretty-nixfmt-wrapper";
 
-        runtimeInputs = [
-          pkgs.coreutils-full
-          pkgs.fd
-          pkgs.nim
-          pkgs.nixfmt
-        ];
+          runtimeInputs = [
+            pkgs.coreutils-full
+            pkgs.fd
+            pkgs.nim
+            pkgs.nixfmt
+          ];
 
-        text = ''
-          realpath "$@"
-          fd "$@" -t f -e nix -x nixfmt '{}'
-          fd "$@" -t f -e nim -x nimpretty '{}'
-        '';
-      };
+          text = ''
+            realpath "$@"
+            fd "$@" -t f -e nix -x nixfmt '{}'
+            fd "$@" -t f -e nim -x nimpretty '{}'
+          '';
+        };
+      });
+
       overlays.default = f: _: { FIXME = f.callPackage ./package.nix { }; };
-      packages.${system}.default = pkgs.callPackage ./package.nix { };
+
+      packages = generateSystems (pkgs: {
+        default = pkgs.callPackage ./package.nix { };
+      });
     };
 }
